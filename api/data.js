10ed99788f5e443f83061ff7e45549d02af0c6ba
Maya Dunlap <76010127+mfdunlap@users.noreply.github.com>
2026-03-30 16:47:58 -0500

    Update data.js
    fix isolation test

--- api/data.js
+++ api/data.js
@@ -39,6 +39,6 @@ const CACHE_TTL = 60 * 1000; // 60 seconds
   }  
 } */
 
-module.export default function handler(req, res) {
+module.exports default function handler(req, res) {
   res.status(200).json({ ok: true });
 }
