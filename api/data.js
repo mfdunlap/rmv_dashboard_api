d48987d802c26dda33607db622f76426e620eb16
Maya Dunlap <76010127+mfdunlap@users.noreply.github.com>
2026-03-30 14:41:16 -0500

    Update data.js
    Add error condition in case fetching data fails

--- api/data.js
+++ api/data.js
@@ -1,6 +1,9 @@
 export default async function handler(req, res) {
-  const response = await fetch(process.env.RMV_API_URL);
-  const data = await response.json();
-
-  res.status(200).json(data);
+  try {
+    const response = await fetch(process.env.RMV_API_URL);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }  
 }
