ad9f4cb08904ca403c6a3cf419160db0502f8b11
Maya Dunlap <76010127+mfdunlap@users.noreply.github.com>
2026-03-30 17:09:52 -0500

    Update data.js
    Remove caching code that isn't working

--- api/data.js
+++ api/data.js
@@ -14,35 +14,6 @@ module.exports = async function handler(req, res) {
     res.status(200).json({
       source: "live",
       data});
-    /*const now = Date.now();
-
-    // Return cached data if fresh
-    if (cache && now - cacheTime < CACHE_TTL) {
-      return res.status(200).json({
-        source: "cache",
-        data: cache
-      });
-    }
-
-    // Fetch fresh data
-    const url = process.env.RMV_API_URL;
-    console.log("URL:", url);
-
-    if (!process.env.SHEETS_API_URL) {
-      throw new Error("Missing SHEETS_API_URL");
-    }
-    
-    const response = await fetch(process.env.RMV_API_URL);
-    const data = await response.json();
-
-    // Update cache
-    cache = data;
-    cacheTime = now;
-    
-    res.status(200).json({
-      source: "live",
-      data
-    });*/
   } catch (err) {
     res.status(500).json({ error: "Failed to fetch data" });
   }  
