7638269f5daba161931fae721ecf314aa8f867a2
Maya Dunlap <76010127+mfdunlap@users.noreply.github.com>
2026-04-02 15:22:13 -0500

    Update data.js
    Make dynamic CORS domain options

--- api/data.js
+++ api/data.js
@@ -2,11 +2,22 @@
 // let cachtTime = 0;
 
 // const CACHE_TTL = 60 * 1000; // 60 seconds
+// Approved origin domain lists
+const allowedOrigins = [
+  "http://localhost:8000",
+  "http://127.0.0.1:8000",
+  "https://respectmovoters.org"
+];
 
 module.exports = async function handler(req, res) {
   try {
-    // CORS headers
-    res.setHeader("Access-Control-Allow-Origin", "https://respectmovoters.org");
+    // Check if origin is in approved domains list
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    
+    // Create remaining CORS headers
     res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
